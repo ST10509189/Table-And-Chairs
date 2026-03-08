31cb0801253925a49b66fed034deac8546e0ed0b
ST10509189 <zilorgat2@gmail.com>
2026-03-08 19:48:13 +0200

    Second Submission

---
+++ src/main/java/com/mycompany/tableandchairs/TableAndChairs.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tableandchairs;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class TableAndChairs {
+
+    public static void main(String[] args) {
+        System.out.println("       **                                   **    ");
+        System.out.println("       **                 $                 **    ");
+        System.out.println("       **                $$$                **    ");
+        System.out.println("       **               $$$$$               **    ");
+        System.out.println("       **         *****************         **    ");
+        System.out.println("       *******    **             **    *******    ");
+        System.out.println("       **   **    **             **    **   **    ");
+        System.out.println("       **   **    **             **    **   **    ");
+        System.out.println("       **   **    **             **    **   **    ");
+        System.out.println("       **   **    **             **    **   **    ");
+       
+        
+    }
+}
